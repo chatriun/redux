7bd48488a29b220dba20021d39a9774393850368
salysaly <anyawee.sr@gmail.com>
2024-05-03 20:27:09 +0700

    fix crateStore, useSelector/useDispatch in Fn component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,26 @@
 import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux"
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
+  const handleIncrement = () => {
+    dispatch({ type: "increment"})
+  }
+  const handleDecrement = () => {
+    dispatch({ type: "decrement"})
+  }
   const toggleCounterHandler = () => {};
 
+  
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <p>
+        <button onClick={handleIncrement}>+</button>
+        <button onClick={handleDecrement}>-</button>
+      </p>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
